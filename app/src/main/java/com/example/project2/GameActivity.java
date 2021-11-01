60384811beda0fc5daafb408b7cd10b756481a3c
rederick <rederick@iastate.edu>
2021-11-01 17:34:15 -0500

    test

--- app/src/main/java/com/example/project2/GameActivity.java
+++ app/src/main/java/com/example/project2/GameActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class GameActivity extends AppCompatActivity {
-
+    //test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
