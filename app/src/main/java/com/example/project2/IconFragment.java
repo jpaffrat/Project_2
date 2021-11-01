5b895e1ae696805bf4ac488f68239a0b97ab91eb
jpaffrat <joe.paffrath@gmail.com>
2021-11-01 17:32:24 -0500

    new fragments

---
+++ app/src/main/java/com/example/project2/IconFragment.java
@@ -0,0 +1,36 @@
+package com.example.project2;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class IconFragment extends Fragment {
+
+    private IconViewModel mViewModel;
+
+    public static IconFragment newInstance() {
+        return new IconFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.icon_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(IconViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/project2/MainViewModel.java
@@ -0,0 +1,7 @@
+package com.example.project2;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
