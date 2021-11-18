72e2df9e3bc2c900bd0ef46bd7da83f7706a4464
jpaffrat <joe.paffrath@gmail.com>
2021-11-18 16:30:03 -0600

    Main Activity fragments are set up with navigation Camera on Icon fragment is working gradle and manifest changed

--- app/src/main/java/com/example/project2/MainActivity.java
+++ app/src/main/java/com/example/project2/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.project2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.view.PreviewView;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentContainerView;
+import androidx.fragment.app.FragmentManager;
+
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private FragmentManager fragmentManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        loadFragment(new MainFragment());
+    }
+
+    public void onIconClicked(View view){
+        loadFragment(new IconFragment());
+    }
+    public void onHighscoresClicked(View view){
+        loadFragment(new HighscoreFragment());
+    }
+    public void onMainClicked(View view){
+        loadFragment(new MainFragment());
+    }
+    public void loadFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.frameLayout,fragment)
+                .commit();
     }
 }
+
